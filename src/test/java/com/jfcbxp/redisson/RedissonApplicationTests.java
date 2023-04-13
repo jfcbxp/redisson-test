56bc34672d24d4a7880aacdbc79d432ab98d7cf8
Joao Braga <jfcbxp@gmail.com>
2023-04-13 11:45:40 -0300

    add expiredEventTest

--- src/test/java/com/jfcbxp/redisson/RedissonApplicationTests.java
+++ src/test/java/com/jfcbxp/redisson/RedissonApplicationTests.java
@@ -5,16 +5,22 @@ import com.jfcbxp.redisson.dto.Student;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.DeletedObjectListener;
+import org.redisson.api.ExpiredObjectListener;
 import org.redisson.api.RBucketReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.client.codec.StringCodec;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+
+import static jodd.util.ThreadUtil.sleep;
 
 @SpringBootTest
 class RedissonApplicationTests {
@@ -70,9 +76,65 @@ class RedissonApplicationTests {
 	void keyValueObjectTest() {
 		Student student = new Student("marshal",10,"atlanta", Arrays.asList(1,2,3));
 		RBucketReactive<Student> bucketReactive = this.client.getBucket("student:1", new TypedJsonJacksonCodec(Student.class));
+		//RBucketReactive<Student> bucketReactive = this.client.getBucket("student:1", JsonJacksonCodec.INSTANCE);
 		Mono<Void> set = bucketReactive.set(student);
 		Mono<Void> get = bucketReactive.get().doOnNext(System.out::println).then();
 		StepVerifier.create(set.concatWith(get)).verifyComplete();
 	}
+
+	@Test
+	void keyValueIncreaseTest() {
+		var atomicLong = this.client.getAtomicLong("user:1:visit");
+		var mono = Flux.range(1,30).delayElements(Duration.ofSeconds(30))
+				.flatMap(i -> atomicLong.incrementAndGet()).then();
+
+		StepVerifier.create(mono).verifyComplete();
+	}
+
+	@Test
+	void bucketAsMapTest() {
+		var mono = this.client.getBuckets(StringCodec.INSTANCE).get("user:1:name","user:2:name")
+				.doOnNext(System.out::println).then();
+		StepVerifier.create(mono).verifyComplete();
+	}
+
+	@Test
+	void expiredEventTest() {
+		RBucketReactive<String> bucketReactive = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+		Mono<Void> set = bucketReactive.set("sam", 10, TimeUnit.SECONDS);
+		Mono<Void> get = bucketReactive.get().doOnNext(System.out::println).then();
+
+		var event = bucketReactive.addListener(new ExpiredObjectListener() {
+			@Override
+			public void onExpired(String s) {
+				System.out.println("expired:" + s);
+			}
+		}).then();
+
+		StepVerifier.create(set.concatWith(get).concatWith(event)).verifyComplete();
+
+		sleep(11000);
+
+	}
+
+	@Test
+	void deletedEventTest() {
+		RBucketReactive<String> bucketReactive = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+		Mono<Void> set = bucketReactive.set("sam");
+		Mono<Void> get = bucketReactive.get().doOnNext(System.out::println).then();
+
+		var event = bucketReactive.addListener(new DeletedObjectListener() {
+			@Override
+			public void onDeleted(String s) {
+				System.out.println("deleted:" + s);
+			}
+		}).then();
+
+		StepVerifier.create(set.concatWith(get).concatWith(event)).verifyComplete();
+
+		sleep(11000);
+
+	}
+
 
 }
